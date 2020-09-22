84aceff1647876cd11840c9a2794d01ffb80547f
Bennett <bennybenester@gmail.com>
2020-09-23 00:05:00 +0300

    :sparkles: add test endpoint for db connection

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,18 @@
-import { Router } from 'express';
-import db from '../database/db'
+import { Router } from "express";
+import db from "../database/db";
 var router = Router();
 
-/* GET home page. */
-router.get('/test', function(req, res, next) {
-  res.json({ title: 'NodeJs Club is awesome' });
+/* Test Endpoint */
+router.get("/test", function (req, res, next) {
+  res.json({ title: "NodeJs Club is awesome" });
+});
+
+/* Test Database Connection */
+router.get("/connection", async (req, res, next)  => {
+  const query = `SELECT nspname FROM pg_catalog.pg_namespace;`;
+  const queries = await db.query(query);
+  console.log(queries)
+  res.json({ schema: queries });
 });
 
 export default router;
